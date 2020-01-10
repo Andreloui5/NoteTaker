fd3fa132a9c1cbda4ae7bb704e21026b2e62d1a1
Craig Gant <craigkgant@gmail.com>
2020-01-10 17:27:53 -0500

    added functionality to api post route

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const db = require("../db/db.json");
+const util = require("util");
 
 //Express = app
 const app = express();
@@ -10,38 +11,60 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 //Middleware extensions
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+//Array for data storage
+let allNotes = [];
 
 //Routes:
 //API routes
 //===================================================================================================
 app.get("/api/notes", (req, res) => {
   res.json(db);
-  // console.log(res);
 });
 
 app.post("/api/notes", (req, res) => {
-  let newNote = req.body;
-  fs.readFile("../db/db.json", (err, data) => {
-    if (err) {
-      throw err;
-    } 
-    content = data
-    console.log(content);
-  })
-  // read the file coming in
-  
-  // get current json article
-  // figure out how to write a unique id to the incoming info
-  // fs.writeFile(__dirname + ) (Etc)
+  // get current json article)
+  fs.readFile("../db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    //parses json data from db.json
+    let oldNotes = JSON.parse(data);
+      //makes a useable array of objects
+      allNotes = oldNotes;
+      // read the data coming in
+      let newId = allNotes.length;
+      let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        //dynamically adds id length
+        id: newId
+      };
+
+      //pushes the user's incoming data into allNotes array
+      allNotes.push(newNote);
+      // console.log(allNotes);
+      res.json(true);
+      //writes new file to db.json
+      fs.writeFile("../db/db.json", JSON.stringify(allNotes), (err) => {
+        if (err) throw err;
+      });
+  });
 });
 
 app.delete("/api/notes/:id", () => {
   //get id (check out the front end/how they got id)
   //read the file
+  fs.readFile("../db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    //parses json data from db.json
+    let oldNotes = JSON.parse(data);
+
+
+      // console.log(allNotes);
+      res.json(true);
+  });
   //do some logic to delete (like a filter)
   //rewrite the file without the deleted info
 });
@@ -56,11 +79,6 @@ app.get("/notes", (req, res) => {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
-
-
-
-
-
 
 
 
